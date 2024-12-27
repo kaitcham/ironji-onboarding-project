810c6fca082f2f5a792d9e4671c271ca9bdaf43a
kaitcham <chamsf5@aol.com>
2024-12-27 09:40:10 +0200

    desktop sidebar

--- components/DashboardSidebar.tsx
+++ components/DashboardSidebar.tsx
@@ -1,3 +1,28 @@
+'use client';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
 export default function DashboardSidebar() {
-  return <div className="dashboard__sidebar">Dashboard Sidebar</div>;
+  const pathname = usePathname();
+  return (
+    <div className="dashboard__sidebar">
+      <div className="dashboard__sidebar__mobile">
+        <h3>Logo</h3>
+      </div>
+      <div className="dashboard__sidebar__desktop">
+        <div className="dashboard__sidebar__desktop__logo">
+          <h3>Logo</h3>
+        </div>
+        <div className="dashboard__sidebar__desktop__menu">
+          <Link
+            href="/dashboard"
+            className={pathname === '/dashboard' ? 'active' : ''}
+          >
+            Shipments
+          </Link>
+          <Link href="/dashboard">User Profile</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
