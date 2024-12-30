0f6a24d8afe9fbfc55aa93338a22b8ef25459ab1
kaitcham <chamsf5@aol.com>
2024-12-30 10:56:24 +0200

    main content header

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,3 +1,10 @@
 export default function page() {
-  return <div className="dashboard__content">Dashboard Content</div>;
+  return (
+    <div className="dashboard__content">
+      <div className="dashboard__content__header">
+        <h3>Shipments</h3>
+        <button>+ Shipment</button>
+      </div>
+    </div>
+  );
 }
